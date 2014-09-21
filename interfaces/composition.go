fbc9b77db1788423e4d0a9d7f37c94f6460cbd72
Shiju Varghese <shiju.varghese@outlook.com>
2014-09-21 13:39:52 +0530

    Added sample for type composition

---
+++ interfaces/composition.go
@@ -0,0 +1,46 @@
+package main
+import "fmt"
+
+type People interface {
+	SayHello()
+	ToString()
+}
+
+type Person struct {
+	name string
+	age int
+	phone string
+}
+
+type Student struct {
+	Person //type embedding for composition
+	university string
+	course string
+}
+type Developer struct {
+	Person //type embedding for composition
+	company string
+	platform string
+}
+//A person method 
+func (p Person ) SayHello() {
+	fmt.Printf("Hi, I am %s, %d yearls old\n", p.name, p.age)
+}
+func (p Person) ToString() {
+	fmt.Printf("[Name: %s, Age: %d, Phone: %s]\n", p.name,p.age, p.phone)
+}
+//Developer's method overrides Person's SayHello
+func (d Developer) SayHello() {
+	fmt.Printf("Hi, I am %s , %d yearls old, developer working on %s for %s\n",
+		d.name,d.age,d.platform,d.company)
+}
+func main() {
+	alex := Student{Person{"alex", 21, "111-222-XXX"}, "MIT","BS CS"}
+	john := Developer{Person{"John", 35, "111-222-XXX"}, "Accel North America", "Golang"}
+	jithesh := Developer{Person{"Jithesh", 33, "111-222-XXX"}, "Accel North America", "Hadoop"}
+	peopleArr := [...]People{alex, john,jithesh}
+	for n, _ := range peopleArr {
+		peopleArr[n].SayHello()
+		peopleArr[n].ToString()
+	}
+}
