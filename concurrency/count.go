92f07bf42c5146dc4851defdc12bf0b4ff1c2e1c
Shiju Varghese <shiju.varghese@outlook.com>
2015-03-06 19:15:30 +0530

    Update and rename concurrency to concurrency/count.go

---
+++ concurrency/count.go
@@ -0,0 +1,40 @@
+// This sample program demonstrates how to create goroutines
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "sync"
+    "time"
+)
+
+// wg is used to wait for the program to finish.
+var wg sync.WaitGroup
+
+func main() {
+
+    // Add a count of two, one for each goroutine.
+    wg.Add(2)
+
+    fmt.Println("Start Goroutines")
+	//launch a goroutine with label "A"
+    go count("A")
+	//launch a goroutine with label "B"
+    go count("B")
+    // Wait for the goroutines to finish.
+    fmt.Println("Waiting To Finish")
+    wg.Wait()
+    fmt.Println("\nTerminating Program")
+}
+
+func count(label string) {
+    // Schedule the call to WaitGroup's Done to tell main we are done.
+    defer wg.Done()
+    // Randomly wait
+    for count := 1; count <= 10; count++ {
+        sleep := rand.Int63n(1000)
+        time.Sleep(time.Duration(sleep) * time.Millisecond)
+        fmt.Printf("Count: %d from %s\n", count, label)
+    }
+}
+
